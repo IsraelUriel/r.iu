4ddc871efd26a30e4938a7b9d0c4b2ab8b830f8f
Israel Uriel <izrael.net@gmail.com>
2022-11-12 22:39:45 -0600

    Update basics.r

--- lessons/basics.r
+++ lessons/basics.r
@@ -48,9 +48,13 @@ length(numeric_vector)
 
 # Populate Vector Via Sequences
 (a <- seq(from=0, to = 10, by = 2))
-(b <- seq(0,10,3))
+(b <- seq(0,10,2))
 (c <- rep(5, times=5))
 (d <- rep(5,5))
+
+# Bindings
+(vector_ab <- cbind(a,b))
+(vector_cd <- rbind(a,b))
 
 # Assign days as names of numeric_vector
 names(numeric_vector) <- c("One", "Two", "Three")
@@ -95,4 +99,76 @@ selected_members_vector
 selection_vector <- A_vector >= 2
 selected_members_vector <- A_vector[selection_vector]
 selected_members_vector
+
+# Matrix
+
+# Construct a matrix with 3 rows that contain the numbers 1 up to 9
+matrix(1:9, byrow = TRUE, nrow=3)
+
+vector_a <- seq(1,5,1)
+vector_b <- seq(6,10,1)
+vector_c <- seq(11,15,1)
+
+vector_union <- c(vector_a, vector_b, vector_c)
+vector_union
+
+matrix_v <- matrix(vector_union, byrow = TRUE, nrow = 3)
+matrix_v
+
+row_names <- c("vector_a", "vector_b", "vector_c")
+col_names <- c("one","two","three","four","five")
+
+rownames(matrix_v) <- row_names
+colnames(matrix_v) <- col_names
+
+matrix_v
+
+# Other Way To Name Matrix
+matrix_v <- matrix(vector_union, byrow = TRUE, nrow = 3, dimnames = list(row_names, col_names))
+matrix_v
+
+# Matrix Dimension
+dim(matrix_v)
+
+# Arithmetic Operations
+vector_matrix_total <- rowSums(matrix_v)
+vector_matrix_total
+
+matrix_v / 5
+matrix_v * 5
+(mean(matrix_v))
+
+
+# Combines
+matrix_ab <- c(vector_a, vector_b)
+matrix_bc <- c(vector_b, vector_c)
+
+row_names_ab <- c("matrix_ab", "matrix_bc")
+col_names_ab <- c("one","two","three","four","five","six","seven","eight","nine","ten")
+matrix_ab <- matrix(cbind(matrix_ab, matrix_bc), byrow = TRUE, nrow = 2, dimnames = list(row_names_ab, col_names_ab))
+
+matrix_ab 
+
+total_matrix_rows = rowSums(matrix_ab)
+total_matrix_cols = colSums(matrix_ab)
+                       
+total_matrix_rows
+total_matrix_cols
+
+# Selections
+
+matrix_v
+
+m_sel_col_2 <- matrix_v[,2]
+m_sel_col_2
+
+m_sel_col_2_4 <- matrix_v[,2:4]
+m_sel_col_2_4
+
+v_a_selection <- ((vector_a %% 2) == 0) & (vector_a > 2)
+v_a_selection 
+
+vector_r <- vector_a[v_a_selection]
+vector_r
+
 
