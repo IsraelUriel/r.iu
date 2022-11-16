f0957c0a77a9390f7ccdcbc30a5310ba96dcb75e
Israel Uriel <izrael.net@gmail.com>
2022-11-15 22:47:36 -0600

    Create challenge_01.r

---
+++ lessons/lesson_02/challenge_01.r
@@ -0,0 +1,38 @@
+"Desarollo
+En este reto vamos a practicar nuestra habilidad en R para leer datos usando que 
+ya hemos utilizado y agregando nuevas a nuestro repertorio
+
+Supongamos que estás interesado en utilizar todos los datos disponibles de la liga 
+española 2019/2020 y formar un DataFrame único de los siguientes archivos:
+"  
+?lapply
+?do.call
+
+library(dplyr)
+
+u1011 <- "https://www.football-data.co.uk/mmz4281/1011/SP1.csv"
+u1112 <- "https://www.football-data.co.uk/mmz4281/1112/SP1.csv"
+u1213 <- "https://www.football-data.co.uk/mmz4281/1213/SP1.csv"
+u1314 <- "https://www.football-data.co.uk/mmz4281/1314/SP1.csv"
+
+# Crea una lista con los nombres de las variables anteriores
+
+lista.archivos <- list(u1011, u1112, u1213, u1314)
+  
+# Con ayuda de la función lapply() lee todos los .csv de lista.archivos. 
+# Usa help('lapply') para saber más de esta función. ¿Qué tipo de objeto 
+# regresa lapply?
+
+lista.csv <- lapply(lista.archivos, read.csv, header = TRUE)
+
+# Ejecuta la siguiente función para sólo tomar un rango de variables. 
+# Más adelante en esta sesión veremos la función select()
+
+lista.datos <- lapply(lista.csv, select, Date:FTR)
+
+# Utiliza la función do.call() para repetir una función que te permite juntar por 
+# filas todos los elementos de lista.datos. Usa help('do.call') para saber más de 
+# esta función.
+
+df <- do.call(rbind, lista.datos)
+head(df)
