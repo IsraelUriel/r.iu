c2c24e269753a01aa8f5456fc81bc9d2fa2ea465
Israel Uriel <izrael.net@gmail.com>
2022-11-16 19:07:02 -0600

    Update challenge_02.r

--- lessons/lesson_02/challenge_02.r
+++ lessons/lesson_02/challenge_02.r
@@ -1,1 +1,37 @@
+"Utilizando el DataFrame del Reto01 de esta sesión, crea una tabla que muestre sólo 
+aquellos equipos que, en total de la liga, hayan metido más del 85% de los goles 
+jugando como local. Muestra sólo las variables HomeTeam y tu variable de proporción 
+y arregla los datos de forma descendente respecto a la proporción de goles."
+
+library(dplyr)
+
+# Setup -------------------------------------------------------------------
+
+u1011 <- "https://www.football-data.co.uk/mmz4281/1011/SP1.csv"
+u1112 <- "https://www.football-data.co.uk/mmz4281/1112/SP1.csv"
+u1213 <- "https://www.football-data.co.uk/mmz4281/1213/SP1.csv"
+u1314 <- "https://www.football-data.co.uk/mmz4281/1314/SP1.csv"
+
+
+lista.archivos <- list(u1011, u1112, u1213, u1314)
+lista.csv <- lapply(lista.archivos, read.csv, header = TRUE)
+lista.datos <- lapply(lista.csv, select, Date:FTR)
+ul_df <- do.call(rbind, lista.datos)
+head(ul_df)
+
+
+# Challenge ---------------------------------------------------------------
+
+#View(ul_df)
+
+# Porcentaje de diferencia de goles entre local y visitantte
+es.teams <- ul_df %>%
+           select(HomeTeam, FTHG, FTAG) %>%
+           group_by(HomeTeam) %>%
+           summarize(hg = sum(FTHG), 
+                     ag = sum(FTAG)) %>%
+           mutate(HGP = (hg - ag) / hg * 100 ) %>%
+           filter(HGP >= 50.0) %>%
+           arrange(desc(HGP)) %>%
+           select(HomeTeam, HGP)
 
