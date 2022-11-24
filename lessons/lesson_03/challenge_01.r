ac8624933473c3ed7858eb1d74e4235a54689dc1
Israel Uriel <izrael.net@gmail.com>
2022-11-24 11:58:30 -0600

    Update challenge_01.r

--- lessons/lesson_03/challenge_01.r
+++ lessons/lesson_03/challenge_01.r
@@ -41,16 +41,69 @@ attributes(diamonds$cut) # Fair, GOod, Very Good, Premium, Ideal
 
 
 # Tabla de Frecuencias Relativa
-diamond_freq <- table(diamonds$cut) 
-transform(diamond_freq, rel = prop.table(diamond_freq))
+freq <- table(diamonds$cut) 
 
-# Very Good % = 22.39
-# 
+table.freq <- transform(freq, rel = prop.table(Freq))
+
+# Frecuencia Relativa Acumulada
+table.freq.acum <- transform(freq, cumsum(prop.table(Freq)))
+
+#Agregarla a la tabla anterior
+diamond.freqs <- transform(freq, 
+                           Relativa = table.freq,
+                           Acumulada = table.freq.acum)
 
 
+# Preguntas:
+diamond.freqs %>%
+  filter(Relativa.Var1 == 'Very Good') %>%
+  summarise(round(sum(Relativa.rel) * 100, 2))
+# Very Good % = 22.4 %
 
-ggplot(diamond_freq, aes(cut)) +
-  geom_bar()
+diamond.freqs %>%
+  filter(Relativa.Var1 %in% c('Fair','Good','Very Good')) %>%
+  summarise(round(sum(Relativa.rel) * 100, 2))
+# Very Good % = 34.48 %
+
+diamond.freqs %>%
+  filter(Relativa.Var1 %in% c('Very Good','Premium','Ideal')) %>%
+  summarise(round(sum(Relativa.rel) * 100, 2))
+# Very Good % = 87.92 %
+
+
+# Determinar Número de Clases
+
+# Formula Sturges
+Sturges = ceiling(1 + 3.3 * log10(length(diamonds$price)))
+
+# Ancho Columna
+width = (max(diamonds$price) - min(diamonds$price)) / Sturges
+
+# Numero de clases
+bins <- seq(min(diamonds$price), max(diamonds$price), by = width)
+
+diamonds.price <- cut(diamonds$price, breaks = bins, include.lowest = TRUE, dig.lab = 10)
+
+# Tabla de Frecuencia
+dist.freq <- table(diamonds.price)
+diamond.freqs <-transform(dist.freq, 
+                       Relativa = prop.table(Freq), 
+                       Acumulada = cumsum(prop.table(Freq)))
+
+# Preguntas
+
+diamond.freqs %>%
+  summarise(round(Relativa[4] * 100, 2))
+# precio entre 3590.17 y 4678.23 : 9.31 %
+
+diamond.freqs %>%
+  summarise(round(Acumulada[7] * 100, 2))
+# precio menor a 7942.41 : 85.75 %
+
+diamond.freqs %>%
+  summarise(round(sum(Relativa[11:17]) * 100 ,2))
+# precio mayor a 11206.58 : 7.54 %
+
 
 
 
