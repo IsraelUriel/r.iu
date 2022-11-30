56a841d0c511405bf40f608eb95e27b0aa61c8b5
Israel Uriel <izrael.net@gmail.com>
2022-11-30 12:28:48 -0600

    Update postwork_05.r

--- lessons/lesson_05/postwork_05.r
+++ lessons/lesson_05/postwork_05.r
@@ -27,6 +27,7 @@ library('ggplot2')
 summary(iris)
 str(iris)
 glimpse(iris)
+View(iris)
 
 # Nivel de Confianza: 99% -> 0.01
 nc <- 0.01
@@ -42,7 +43,109 @@ x <- iris$Sepal.Length
 # Ha: mu != 5.7
 mu <- 5.7
 
-t.test(x, alternative = 'two.sided', mu)
+p.value <- t.test(x, alternative = 'two.sided', mu = mu)$p.value
+t.mean <- t.test(x, alternative = 'two.sided', mu = mu)$estimate
 
 
-p.value <- t.test(x, alternative = 'greater', mu)$p_value
+if (p.value >= nc) { 
+  cat('R: No Se Rechaza Ho:', p.value, '>=', nc)
+} else {
+  cat('R: Se Rechaza: Ho', p.value, '<', nc)
+} 
+
+cat('Diferencia de medias:', 't.mean:', t.mean, '|', 'mu:', mu)
+
+# A nivel de confianza estándar de 99%, no existe evidencia estadística 
+# para rechazar Ho, es decir El promedio no es distinto a 5.7.
+
+
+# B) ----------------------------------------------------------------------
+
+# En promedio, el ancho del pétalo de la especie virginica (Petal.Width) es menor a 2.1 cm
+
+x <- iris$Petal.Width
+
+# Hipótesis:
+# Ho: mu >= 2.1
+# Ha: mu < 2.1
+mu <- 2.1
+
+p.value <- t.test(x = x, alternative = 'less', mu = mu)$p.value
+t.mean <- t.test(x = x, alternative = 'less', mu = mu)$estimate
+
+
+if (p.value >= nc) { 
+  cat('R: No Se Rechaza Ho:', p.value, '>=', nc)
+} else {
+  cat('R: Se Rechaza: Ho', p.value, '<', nc)
+} 
+
+cat('Diferencia de medias:', 't.mean:', t.mean, '|', 'mu:',mu)
+
+# A nivel de confianza estándar de 99%, existe evidencia estadística 
+# para rechazar Ho, es decir El promedio es menor distinto a 2.1.
+
+
+# C) ----------------------------------------------------------------------
+
+# En promedio, el largo del pétalo de la especie virgínica es 1.1 cm más grande que el promedio 
+# del largo del pétalo de la especie versicolor.
+
+x <- unlist(iris[iris$Species == 'virginica', 'Petal.Length'], use.names = FALSE) 
+y <- unlist(iris[iris$Species == 'versicolor', 'Petal.Length'], use.names = FALSE)
+
+# Hipótesis:
+# Ho: mu <= 1.1
+# Ha: mu > 1.1
+mu <- 1.1
+
+var.test(x, y, alternative = 'greater', ratio = 1.1)
+
+p.value <- t.test(x, y, alternative = 'greater', mu = 0, var.equal = FALSE)$p.value
+t.mean <- t.test(x, y, alternative = 'greater', mu = 0, var.equal = FALSE)$estimate
+
+if (p.value >= nc) { 
+  cat('R: No Se Rechaza Ho:', p.value, '>=', nc)
+} else {
+  cat('R: Se Rechaza: Ho', p.value, '<', nc)
+} 
+
+cat('Diferencia de medias:', 't.mean:', t.mean, '|', 'mu:',mu)
+
+# A nivel de confianza estándar de 99%, existe evidencia estadística 
+# para rechazar Ho, es decir El promedio es mayor que 1.1.
+
+# D) ----------------------------------------------------------------------
+
+# En promedio, no existe diferencia en el ancho del sépalo entre las 3 especies.
+
+# Hipótesis:
+# mu0 = mu1 = mu2
+# mu0 != mu1 != mu2
+
+boxplot(iris$Sepal.Width ~ factor(iris$Species),
+        xlab = "Species", ylab = "Width")
+
+x <- unlist(iris[iris$Species == 'virginica', 'Sepal.Width'], use.names = FALSE) 
+y <- unlist(iris[iris$Species == 'versicolor', 'Sepal.Width'], use.names = FALSE)
+z <- unlist(iris[iris$Species == 'virginica', 'Sepal.Width'], use.names = FALSE)
+
+samples <- data.frame(y = c(x,y,z),
+  group = c('virginica', 'versicolor', 'setosa'))
+
+m <- lm(y ~ group, data = samples)
+anova(m)
+summary(m)
+p.value <- anova(m)$'Pr(>F)'[1]
+
+if (p.value >= nc) { 
+  cat('R: No Se Rechaza Ho:', p.value, '>=', nc)
+} else {
+  cat('R: Se Rechaza: Ho', p.value, '<', nc)
+} 
+
+par(mfrow=c(2, 2))
+plot(m)
+
+# A nivel de confianza estándar de 99%, existe evidencia estadística 
+# para No rechazar Ho, es decir El promedio es igual entre las 3 Especies.
