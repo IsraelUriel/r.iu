94ef58241b35292699cb6d660a93ba52819d6986
Israel Uriel <izrael.net@gmail.com>
2022-12-02 16:00:33 -0600

    Update postwork_06.r

--- lessons/lesson_06/postwork_06.r
+++ lessons/lesson_06/postwork_06.r
@@ -16,3 +16,48 @@ X3: Newspaper (Presupuesto de publicidad en Periódico para el producto)
 
 
 # Solution: ---------------------------------------------------------------
+
+library('dplyr')
+
+df <- read.csv('advertising.csv', header = TRUE)
+str(df)
+glimpse(df)
+
+# Data Clean Up
+# Selecting Variables
+sample <- select(df, Sales, TV, Radio, Newspaper)
+
+# Using Correlation
+
+# Matrix Correlation
+round(cor(sample),4) # -TV- var presenta el valor mas cercano a 1
+
+pairs(~ Sales + TV + Radio + Newspaper,
+      data = sample, gap = 0.4, cex.labels = 1.5)
+# La gráfica muesta que la relación Sales con TV presenta correlacion positiva 
+
+# Model
+attach(sample)
+m1 <- lm(Sales ~ TV + Radio + Newspaper)
+summary(m1)
+
+# Mediana cercana a 1 = Modelo confiable
+# R2 Ajustada cercana a 1 = Indica buen ajuste del Modelo
+
+nc <- 0.05 # Usando nivel de confianza = 0.05
+
+
+# Get Sig Variables < sig
+sig.m1 <- summary(m1)$coeff[-1,4] < nc
+names(sig.m1)[sig.m1 == TRUE]
+# Variables TV y Radio favorables al nivel de confianza
+
+# Model Relations for selected sigs
+
+y <- sample$Sales
+x1 <- sample$TV
+x2 <- sample$Radio
+
+# M2: Sales ~ TV
+m2 <- update(m1, Sales ~.- Radio - Newspaper)
+summary(m2)
